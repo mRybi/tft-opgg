05fe7a6104b09d91e4947e51a7201198ce35cc12
Mikolaj <m.rybinski@weegree.com>
2020-05-03 17:05:22 +0200

    Backend Express.js server for retriving data from Riot API

---
+++ backend/routes/match.js
@@ -0,0 +1,52 @@
+const got = require('got');
+var express = require('express');
+
+var router = express.Router();
+
+const EUW_API_BASE_URL = 'https://euw1.api.riotgames.com/tft/';
+const GET_SUMMONER_BY_NAME = 'summoner/v1/summoners/by-name/';
+const GET_SUMMONER_LEAGUE = 'league/v1/entries/by-summoner/';
+const GET_LEAGUE_INFO = 'league/v1/leagues/';
+
+const EUW_API_BASE_MATCH_URL = 'https://europe.api.riotgames.com/tft/match/v1/matches/';
+const GET_SUMMONER_MATCHES = 'by-puuid/';
+
+const getPieczarson = `${EUW_API_BASE_URL}${GET_SUMMONER_BY_NAME}Fat%20Pie%20Char%20Son`;
+const getPieczarsonLeague = `${EUW_API_BASE_URL}${GET_SUMMONER_LEAGUE}rZRWxIRzNqpec7h4tiJdeE64bN62dsns6M9BuUpjc56dyof9`;
+const getPieczarsonLeagueInfo = `${EUW_API_BASE_URL}${GET_LEAGUE_INFO}84b5efae-a262-4706-9678-131b286cbee4`;
+const getPieczarsonMatches = `${EUW_API_BASE_MATCH_URL}${GET_SUMMONER_MATCHES}GlKsb_V0ctTMi0sveOsqm_-E6H702K4SF_vtp0bg8s1TDZHUQayKKAdeueuNM2KHZw2ZFYRRvgyiYA/ids?count=${20}`;
+const getPieczarsonMatchInfo = `${EUW_API_BASE_MATCH_URL}EUW1_4558360996`;
+
+
+/* GET users listing. */
+router.get('/by-summoner/:id', async function (req, res, next) {
+  try {
+    const query = req.params.id;
+    const response = await got(`${EUW_API_BASE_MATCH_URL}${GET_SUMMONER_MATCHES}${query}/ids?count=${20}`, {
+      headers: {
+        'X-Riot-Token': 'RGAPI-07861a03-575a-4f9e-9c52-f7b4e48e88fe'
+      }
+    }).json();
+    res.send(response);
+
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+router.get('/info/:id', async function (req, res, next) {
+  try {
+    const query = req.params.id;
+    const response = await got(`${EUW_API_BASE_MATCH_URL}${query}`, {
+      headers: {
+        'X-Riot-Token': 'RGAPI-07861a03-575a-4f9e-9c52-f7b4e48e88fe'
+      }
+    }).json();
+    res.send(response);
+
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = router;
